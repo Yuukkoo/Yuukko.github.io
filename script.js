b212233b5948732323c5a7d81c91f1a9a5105765
Yuukkoo <113667849+Yuukkoo@users.noreply.github.com>
2024-10-19 17:38:40 +0300

    suicide

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const gifButton = document.getElementById('gifButton');
-    const gifContainer = document.getElementById('gifContainer');
+document.getElementById('openEnvelopeBtn').addEventListener('click', function() {
+    document.querySelector('.envelope').classList.add('open');
+});
 
-    const funnyGifs = [
-        "https://media.giphy.com/media/3o6Zt481isNVuQI1l6/giphy.gif", 
-        "https://media.giphy.com/media/l0HlPjezYYVwnInUA/giphy.gif",
-        "https://media.giphy.com/media/26n6WywJyh39n1pBu/giphy.gif",
-    ];
-
-    gifButton.addEventListener('click', function() {
-        const randomIndex = Math.floor(Math.random() * funnyGifs.length);
-        const gifUrl = funnyGifs[randomIndex];
-
-        gifContainer.innerHTML = `<img src="${gifUrl}" alt="Funny GIF">`;
-    });
+document.getElementById('closeEnvelopeBtn').addEventListener('click', function() {
+    document.querySelector('.envelope').classList.remove('open');
 });
